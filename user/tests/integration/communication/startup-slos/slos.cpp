98aaa4be6ecbc399d493271b902ebddabcd9f511
Joe Goggins <joe.goggins@gmail.com>
2021-02-02 18:10:28 -0600

    Show free_mem in test output whether pass or fail

--- user/tests/integration/communication/startup-slos/slos.cpp
+++ user/tests/integration/communication/startup-slos/slos.cpp
@@ -1,9 +1,11 @@
 #include "application.h"
 #include "test.h"
 
-test(slo_startup_stats) {
+test(slo_startup_stats)
+{
     Particle.connect();
     waitUntil(Particle.connected);
     uint32_t free_mem = System.freeMemory();
-    assertMoreOrEqual(free_mem, 60000);
+    String stats = String::format("{\"free_mem\": %u }", free_mem);
+    Particle.publish("startup_stats", stats);
 }
--- user/tests/integration/communication/startup-slos/slos.spec.js
+++ user/tests/integration/communication/startup-slos/slos.spec.js
@@ -1,3 +1,10 @@
 suite('DeviceOS Startup Service Level Objectives (SLOs)');
 
 platform('gen2', 'gen3');
+
+test('slo startup stats', async function () {
+    const unparsedJson = await this.particle.receiveEvent('startup_stats');
+    const startupStats = JSON.parse(unparsedJson);
+    console.log(`free_mem=${startupStats.free_mem}`);
+    expect(startupStats.free_mem).to.be.at.least(60000);
+});
